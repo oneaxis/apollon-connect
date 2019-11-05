bf094d24f71886b8ff12e7cffc381882d9299654
timoweber <timo_weber@outlook.com>
2019-11-05 15:54:23 +0100

    Initial commit with Spring Boot base

---
+++ src/main/java/de/oneaxis/apollon/connect/ApollonConnectApplication.java
@@ -0,0 +1,13 @@
+package de.oneaxis.apollon.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApollonConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApollonConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/oneaxis/apollon/connect/ApollonConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.oneaxis.apollon.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApollonConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
