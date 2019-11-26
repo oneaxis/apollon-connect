84e266b32ee64e8c7fe45a64ffa97d405ee1e7c6
Timo <timo.weber@fincon.eu>
2019-11-26 15:40:44 +0100

    f/10 - fixed serialization issues

--- src/test/java/de/oneaxis/apollon/connect/application/musician/MusicianControllerTests.java
+++ src/test/java/de/oneaxis/apollon/connect/application/musician/MusicianControllerTests.java
@@ -1,9 +1,7 @@
 package de.oneaxis.apollon.connect.application.musician;
 
 import de.oneaxis.apollon.connect.model.band.Band;
-import de.oneaxis.apollon.connect.model.band.BandId;
 import de.oneaxis.apollon.connect.model.musician.Musician;
-import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
