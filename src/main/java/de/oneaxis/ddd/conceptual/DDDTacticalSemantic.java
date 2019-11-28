5534c0875e40da27360dab21b1ed4d89375d9a79
Timo <timo.weber@fincon.eu>
2019-11-28 12:23:06 +0100

    f/10 - extend java docs

--- src/main/java/de/oneaxis/ddd/conceptual/DDDTacticalSemantic.java
+++ src/main/java/de/oneaxis/ddd/conceptual/DDDTacticalSemantic.java
@@ -2,9 +2,13 @@ package de.oneaxis.ddd.conceptual;
 
 import java.lang.annotation.*;
 
+/**
+ * A convenience annotation to declare an element as a tactical semantic element for usage in
+ * a domain driven design scope.
+ */
 @Documented
-@Target(ElementType.TYPE)
+@Target(ElementType.ANNOTATION_TYPE)
 @Retention(RetentionPolicy.SOURCE)
-public @interface DDDTacticalSemantic {
+@interface DDDTacticalSemantic {
 
 }
